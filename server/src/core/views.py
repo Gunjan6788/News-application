d0d84571127f9da4fdd18730e48ce6361a2144a6
Gunjan6788 <gunjan6788@gmail.com>
2020-08-30 18:10:56 +0530

    added try catch block in server views

--- server/src/core/views.py
+++ server/src/core/views.py
@@ -12,37 +12,43 @@ def home(request):
 
 
 def top_headlines(request):
-    country = request.GET['country']
-    category = request.GET['category']
-    # return JsonResponse(country, safe=False)
-    url = 'http://newsapi.org/v2/top-headlines'
-    headers = {"Accept": "application/json"}
+    try:
+        country = request.GET['country']
+        category = request.GET['category']
+        # return JsonResponse(country, safe=False)
+        url = 'http://newsapi.org/v2/top-headlines'
+        headers = {"Accept": "application/json"}
 
-    payload = {
-        "country": country,
-        "category": category,
-        "apiKey": "59e8c0a2dd2e41c2b8c96a071a3d5c43"
-    }
-    req = requests.get(url, params=payload, headers=headers)
-    data = req.json()
+        payload = {
+            "country": country,
+            "category": category,
+            "apiKey": "59e8c0a2dd2e41c2b8c96a071a3d5c43"
+        }
+        req = requests.get(url, params=payload, headers=headers)
+        data = req.json()
 
-    return JsonResponse(data)
+        return JsonResponse(data)
+    except Exception as err:
+        return json.dumps({'error': True, 'message': format(err)})
 
 
 def search_news(request):
-    q = request.GET['q']
-    from_date = request.GET['from']
+    try:
+        q = request.GET['q']
+        from_date = request.GET['from']
 
-    url = 'http://newsapi.org/v2/everything'
-    headers = {"Accept": "application/json"}
+        url = 'http://newsapi.org/v2/everything'
+        headers = {"Accept": "application/json"}
 
-    payload = {
-        "q": q,
-        "from": from_date,
-        "sortBy": "publishedAt",
-        "apiKey": "59e8c0a2dd2e41c2b8c96a071a3d5c43"
-    }
-    req = requests.get(url, params=payload, headers=headers)
-    data = req.json()
+        payload = {
+            "q": q,
+            "from": from_date,
+            "sortBy": "publishedAt",
+            "apiKey": "59e8c0a2dd2e41c2b8c96a071a3d5c43"
+        }
+        req = requests.get(url, params=payload, headers=headers)
+        data = req.json()
 
-    return JsonResponse(data)
+        return JsonResponse(data)
+    except Exception as err:
+        return json.dumps({'error': True, 'message': format(err)})
